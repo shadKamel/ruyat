ceafd548dbeadc723ec5e2df4e5181b3ace1833c
shad kamil <kamel.shad.sk@gmail.com>
2025-09-01 11:19:55 +0300

    slaw

--- src/components/Home/everything-you-need.tsx
+++ src/components/Home/everything-you-need.tsx
@@ -18,7 +18,7 @@ const cards = [
 
 export default function EverythingYouNeed() {
     return (
-        <section className="mx-auto max-w-5xl py-24 sm:py-28 md:overflow-x-hidden lg:overflow-x-visible">
+        <section className="mx-auto max-w-5xl overflow-x-hidden py-24 sm:py-28 lg:overflow-x-visible">
             <SectionHeading
                 tag="Everything you need"
                 title="Streamlined for easy management"
